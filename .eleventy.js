40e4babd6c4355ea4bb80aefa7bd7ef7cf131a50
Efer482 <64799814+Efer482@users.noreply.github.com>
2024-06-25 00:21:04 -0500

    test

--- .eleventy.js
+++ .eleventy.js
@@ -76,7 +76,7 @@ fileName = fullPath + "1";
       attributes: {
         "class": "internal-link is-unresolved",
         "href": "/404",
-        "target": fileName,
+        "target": filePath,
       },
       innerHTML: title,
     }
